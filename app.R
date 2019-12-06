a2c989588c136606a4c9e68ce860005c6fa3df62
Ray <44300713+RomeoAlphaYankee@users.noreply.github.com>
2019-12-06 11:38:59 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -5,20 +5,34 @@ library(shiny)
 data("gapminder")
 
 ui <- fluidPage(
-  selectInput(inputId = "year", 
-              label = "Choose a Year", 
-              choices = as.numeric(names(table(gapminder$year))),
-              selected = 2007),
-  plotOutput("plot"))
+  titlePanel(title = "Gapminder Dataset"),
+  sidebarPanel(
+    selectInput(inputId = "year", 
+                label = "Choose a Year", 
+                choices = as.numeric(names(table(gapminder$year))),
+                selected = 2007),
+    tableOutput("table")),
+  mainPanel(
+    plotOutput("plot"))
+  )
 
 server <- function(input, output) {
+  
+  output$table <- renderTable({
+    gapminder %>%
+      filter(year == input$year) %>%
+      group_by(continent) %>%
+      summarize(GDPperCap = mean(gdpPercap), LifeExp = mean(lifeExp)) %>%
+      arrange(desc(LifeExp))
+  })
+  
   output$plot <- renderPlot({
     gapminder %>%
       filter(year == input$year) %>%
       ggplot(aes(x = gdpPercap, y = lifeExp, color = continent)) +
       geom_point(aes(size = pop)) +
       scale_x_continuous(trans = "log10") +
-      labs(x = "GDP Per Capita", y = "Life Expectancy", title = "Life Expectancy as a Function of GDP Per Capita", color = "Continent", size = "Population")
+      labs(x = "GDP Per Capita (log10 scale)", y = "Life Expectancy", title = "Life Expectancy as a Function of GDP Per Capita", color = "Continent", size = "Population")
     })
 }
 
