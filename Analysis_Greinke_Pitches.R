4b7d1276a8eda844075600101dae492daa5a2454
Ray <44300713+RomeoAlphaYankee@users.noreply.github.com>
2019-12-09 17:19:26 -0500

    Add files via upload

---
+++ Analysis_Greinke_Pitches.R
@@ -0,0 +1,138 @@
+# Pitch Analysis of Zach Greinke's 2015 season
+# This analysis will cover pitch types, velocity over time, locations, etc..
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(lubridate)
+
+# Load the data on Zack Greinke 2015 season
+greinke <- read_csv("https://assets.datacamp.com/production/course_943/datasets/greinke2015.csv")
+
+head(greinke)
+dim(greinke)
+
+# Check for missing data
+colSums(apply(greinke, 2, FUN = is.na))
+summary(greinke)
+
+greinke[is.na(greinke$break_angle), ]
+greinke[is.na(greinke$start_speed), ]
+
+# Clean up the three pitches with NA data
+greinke <- subset(greinke, subset = !is.na(greinke$start_speed))
+
+# Check structure
+str(greinke)
+
+# Correct dates
+greinke$game_date <- mdy(greinke$game_date)
+
+class(greinke$game_date)
+
+# Separate the months
+greinke <- separate(data = greinke, col = game_date, into = c("year", "month", "day"), sep = "-", remove = FALSE)
+greinke$month <- as.numeric(greinke$month)
+
+# Isolate the month of July
+greinke$july <- ifelse(greinke$month == 7, "july", "other")
+
+# Check results
+head(greinke)
+summary(factor(greinke$july))
+
+# Plot pitch speeds
+summary(greinke$start_speed)
+
+greinke %>%
+  ggplot(aes(x = start_speed)) +
+  geom_histogram(binwidth = 1)
+
+# Clearly multi-modal
+# Plot by pitch type
+greinke %>%
+  ggplot(aes(x = start_speed)) +
+  geom_histogram() +
+  facet_wrap(~pitch_type)
+
+# Plot speeds of main pitches in the same plot
+greinke %>%
+  filter(pitch_type %in% c("FF", "FT","SL", "CH", "CU")) %>%
+  ggplot(aes(x = start_speed, fill = pitch_type)) +
+  geom_bar(aes(color = pitch_type), position = "dodge", alpha = 0.4)
+
+# Examine the mean pitch speed
+greinke %>% 
+  filter(pitch_type %in% c("FF", "FT","SL", "CH", "CU")) %>%
+  group_by(pitch_type) %>%
+  summarize(mean_speed = mean(start_speed))
+
+# Examine the four seam fastball velocity in more detail
+greinke %>%
+  filter(pitch_type == "FF") %>%
+  group_by(month) %>%
+  summarize(mean_velocity = mean(start_speed))
+
+# Create a boxplot by month
+greinke %>%
+  filter(pitch_type == "FF") %>%
+  group_by(month) %>%
+  ggplot(aes(x = factor(month), y = start_speed)) +
+  geom_boxplot(aes(group = month)) +
+  labs(x = "Month", y = "Velocity (MPH)", title = "Greinke four-seam fastball speed by month")
+
+# It looks like fastball velocity improved from the beginning of the season onward
+# Lets examine the month of July more closely when veolocity began to peak
+july_ff <- subset(x = greinke, subset = pitch_type == "FF" & month == 7)
+other_ff <- subset(x = greinke, subset = pitch_type == "FF" & month != 7)
+
+# Make a fastball speed histogram for other months
+hist(other_ff$start_speed,
+     col = "#00009950", freq = FALSE,
+     ylim = c(0, .35), xlab = "Velocity (mph)",
+     main = "Greinke 4-Seam Fastball Velocity")
+
+# Add a histogram for July
+hist(july_ff$start_speed, add = TRUE, col = "#99000050", freq = FALSE)
+
+# Draw vertical lines at the means of the two fastball histograms
+abline(v = mean(other_ff$start_speed), col = "#00009950", lwd = 2)
+abline(v = mean(july_ff$start_speed), col = "#99000050", lwd = 2)
+
+# Table average four-seam fastball velocity by month
+monthAvg <- data.frame(tapply(X = greinke$start_speed, INDEX = greinke$month, FUN = mean))
+monthAvg[[2]] <- tapply(X = greinke$start_speed, INDEX = greinke$month, FUN = median)
+names(monthAvg) <- c("mean", "median")
+monthAvg
+
+# Look at the four-seam fastball velocity by game
+greinke_ff <- subset(greinke, subset = pitch_type == "FF")
+
+ff_dt <- data.frame(tapply(greinke_ff$start_speed, greinke_ff$game_date, mean))
+head(ff_dt)
+
+ff_dt$game_date <- ymd(rownames(ff_dt))
+colnames(ff_dt) <- c("start_speed", colnames(ff_dt)[-1])
+row.names(ff_dt) <- NULL
+head(ff_dt)
+
+# Plot game-by-game 4-seam fastballs
+plot(ff_dt$start_speed ~ ff_dt$game_date,
+     lwd = 4, type = "l", ylim = c(88, 95),
+     main = "Greinke 4-Seam Fastball Velocity",
+     xlab = "Date", ylab = "Velocity (MPH)")
+
+# Add the individual pitches
+points(greinke_ff$start_speed ~ jitter(as.numeric(greinke_ff$game_date)), pch = 16, col = "#99004450")
+
+
+### Explore pitch mix in greater detail
+# Lets start by removing the one or two eephus pitches and intentional balls
+greinke <- greinke[-c(which(greinke$pitch_type == "EP" | greinke$pitch_type == "IN")), ]
+
+table(greinke$pitch_type, greinke$month)
+round(prop.table(table(greinke$pitch_type, greinke$month), margin = 2), 3)
+
+# Specifically look at the proportion of pitches in July vs. all other months combined
+round(prop.table(table(greinke$pitch_type, greinke$july), margin = 2), 3)
+
+
